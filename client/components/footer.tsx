dd9faf5d3bddce87ca8494d70de6f917941662b2
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-14 20:16:24 -0400

    added footer

---
+++ client/components/footer.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Footer(): JSX.Element {
+  return (
+    <div className="absolute inset-x-0 bottom-0">
+      <footer className="w-screen p-4 bg-white rounded-lg shadow md:items-center md:justify-between dark:bg-gray-800 w-100">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{' '}
+          <Link
+            href="https://ethglobal.com/"
+            className="hover:underline"
+            target="_blank"
+          >
+            ETHGlobal
+          </Link>
+          .  All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 sm:mt-0">
+          <li>
+            <Link
+              href="https://ethglobal.com/about"
+              className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+              target="_blank"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://ethglobal.com/rules"
+              className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+              target="_blank"
+            >
+              Rules & Code of Conduct
+            </Link>
+          </li>
+          <li>
+            <a
+              href="https://ethglobal.com/privacy"
+              className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+              target="_blank"
+            >
+              Privacy Policy
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,12 +3,16 @@ import { ApolloProvider } from '@apollo/client'
 import client from '../apollo-client'
 import type { AppProps } from 'next/app'
 import { GlobalContextProvider } from '@/context/context'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalContextProvider>
       <ApolloProvider client={client}>
+        <Header />
         <Component {...pageProps} />
+        <Footer />
       </ApolloProvider>
     </GlobalContextProvider>
   )
